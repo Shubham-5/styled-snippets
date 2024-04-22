eeb4f4596a370dd278f5f2cd9f77e1ec6d71fa88
shubham girase <sgirase@gmail.com>
2024-04-22 13:13:32 +0530

    Remove unused react declaration

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 import Header from "./components/Header";
 
 const Layout = (props: PropsWithChildren) => {
--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Wrapper = () => {
   return <div></div>;
 };
