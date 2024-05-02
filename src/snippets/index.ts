2a560cfe7fd28a64788cd5f6a30ac982b3140c6d
Shubham Girase <sgirase08@gmail.com>
2024-05-02 11:20:45 +0530

    Add navbars snippet

--- src/snippets/index.ts
+++ src/snippets/index.ts
@@ -1,4 +1,5 @@
 import { Buttons } from "./Buttons";
+import { Navbars } from "./Navbars";
 import { Progress } from "./Progress";
 
-export const Snippets = [...Buttons, ...Progress];
+export const Snippets = [...Buttons, ...Navbars, ...Progress];
