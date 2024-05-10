5fe5a294d6966b3cb06b089931f41629a2c5f62f
Shubham Girase <sgirase08@gmail.com>
2024-05-10 14:43:16 +0530

    Add inputs snippet

--- src/snippets/index.ts
+++ src/snippets/index.ts
@@ -1,6 +1,13 @@
 import { Buttons } from "./Buttons";
 import { CheckBoxes } from "./CheckBoxes";
+import { Inputs } from "./Inputs";
 import { Loaders } from "./Loaders";
 import { Navbars } from "./Navbars";
 
-export const Snippets = [...Buttons, ...Loaders, ...CheckBoxes, ...Navbars];
+export const Snippets = [
+  ...Buttons,
+  ...Inputs,
+  ...Loaders,
+  ...CheckBoxes,
+  ...Navbars,
+];
